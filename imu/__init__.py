7991a0dc683e37e4f3e142adefef42d5c0320e5e
jiesou <84175239+jiesou@users.noreply.github.com>
2024-09-05 19:36:00 +0800

    feat: Screw tightening init process

--- imu/__init__.py
+++ imu/__init__.py
@@ -4,8 +4,8 @@ from .communication import read_data
 screw_plan = [
     { "x": 0.1, "y": 0 },
     { "x": 0.18, "y": 0 },
-    { "x": 0.2, "y": 0.1 },
     { "x": 0.18, "y": 0.1 },
+    { "x": 0.1, "y": 0.1 },
 ]
 
 
@@ -20,10 +20,24 @@ def isSameSign(a, b):
 
 def locateScrew(data, plan, positions):
     if len(positions) < 2: return False
-    x_aligned = abs(positions[-1][0]-plan['x']) < 0.1
-    y_aligned = abs(positions[-1][1]-plan['y']) < 0.1
-    print(f"x_aligned: {positions[-1][0]-plan['x']}, y_aligned: {positions[-1][1]-plan['y']}")
+    position_x = positions[-1][0] if abs(positions[-1][0]) > 0.08 else 0
+    position_y = positions[-1][1] if abs(positions[-1][1]) > 0.08 else 0
+    x_aligned = isSameSign(position_x, plan['x']) 
+    y_aligned = isSameSign(position_y, plan['y'])
     return x_aligned and y_aligned
+     
+inited = False
+def atInitialPosition(data):
+    # 斜着放，即初始位置
+    global inited
+    isStanding = data['offset']['x'] == 0 and data['offset']['y'] == 0 and data['offset']['z'] == 0
+    at_initial_position = isStanding and abs(data['angle']['y']+47)<7 and abs(data['angle']['x'])<20 and abs(data['angle']['z'])<20
+    if not inited and at_initial_position:
+        inited = True
+    return at_initial_position
+
+
+
 
 # 1.累计偏移量到地图位置
 def accumulate_offset_to_position(offset, positions, standing):
@@ -38,20 +52,18 @@ def accumulate_offset_to_position(offset, positions, standing):
     new_position[0] = standing[0] + offset['x']
     new_position[1] = standing[1] + offset['y']
     
-    print("new_position: %.3f, %.3f, %.3f; offset: %.3f, %.3f, %.3f" % (new_position[0], new_position[1], new_position[2], offset['x'], offset['y'], offset['z']))
-    positions.append(new_position)
     return new_position
 
 last_trigger_time = 0
 screw_tightening = False
 # 2.判断是否符合拧螺丝要求
 def requirement_process(data, previous_data, positions):
-    global screw_plan, last_trigger_time, screw_tightening
+    global screw_plan, last_trigger_time, screw_tightening, inited
     turningPreviousData(previous_data, data)
     if len(previous_data) < 10: return False, False, False
     
     # 保持 X、Y 轴稳定
-    xy_stable = all(abs(data['gravity_accel']['x']) < 1.5 and abs(data['gravity_accel']['y']) < 1.5 for data in previous_data)
+    xy_stable = all(abs(data['gravity_accel']['x']) < 2 and abs(data['gravity_accel']['y']) < 2 for data in previous_data)
 
     # 保持方向朝下
     aligned = abs(data['angle']['x']) < 25 and abs(data['angle']['y']) < 25
@@ -60,11 +72,12 @@ def requirement_process(data, previous_data, positions):
     located = locateScrew(data, screw_plan[0], positions)
     print(f"located: {located}, xy_stable: {xy_stable}, aligned: {aligned}, screw_plan: {screw_plan[0]}, left: {len(screw_plan)}")
     if located and xy_stable and aligned:
-        print(screw_tightening)
-        if screw_tightening:
+        print(inited)
+        if screw_tightening and inited:
             # 4. 拧螺丝
             screw_plan.pop(0)
             print(f"screwd, {len(screw_plan)} left")
+            inited = False
             if len(screw_plan) < 1:
                 print("done")
                 exit()
@@ -81,6 +94,10 @@ def parse_data():
     for data in read_data():
         if data is None: continue
         new_position = accumulate_offset_to_position(data['offset'], positions, standing)
+        # 初始位置重置坐标系
+        if atInitialPosition(data): new_position = [0, 0, 0]
+        print("new_position: %.3f, %.3f, %.3f" % (new_position[0], new_position[1], new_position[2]))
+        positions.append(new_position)
         located, xy_stable, aligned = requirement_process(data, previous_data, positions)
         yield f"located: {located}, xy_stable: {xy_stable}, aligned: {aligned}, screw_plan: {screw_plan[0]}, left: {len(screw_plan)}\n"
 
---
+++ server/__init__.py
@@ -0,0 +1,36 @@
+from flask import Flask, send_from_directory, stream_with_context, request, g
+from werkzeug.exceptions import HTTPException
+import os
+from .units import res
+from .api import api_bp
+
+app = Flask(__name__)
+
+app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024 * 30
+# 最大上传大小 30M
+
+app.register_blueprint(api_bp, url_prefix='/api')
+
+
+@app.errorhandler(HTTPException)
+def http_error(e):
+    response = res(app, {
+        'code': e.code,
+        'message': e.name})
+    response.status = e.code
+    return response
+
+@app.route('/')
+def index():
+    return send_from_directory('static', 'index.html')
+
+@app.route('/<path:file>')
+def static_ui(file):
+    return send_from_directory('static', file)
+
+@app.route('/api')
+def api_index():
+    return res(app, {
+        'status': 'running',
+        'version': '1.0.0'})
+
