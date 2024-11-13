0f3c967b39a5c1d3be9095b8f9ac7c3594fded67
jiesou <84175239+jiesou@users.noreply.github.com>
2024-11-13 13:00:02 +0800

    feat: 修复电流传感器无法断连

--- current/communication.py
+++ current/communication.py
@@ -6,7 +6,7 @@ def read_data(timeout=1):
     buffer = ""
     while True:  # 外层无限循环用于重连
         try:
-            with serial.Serial("/dev/ttyUSB0", 9600, timeout=timeout) as ser:
+            with serial.Serial("/dev/ttyUSB1", 9600, timeout=timeout) as ser:
                 print("电流检测原件串口已连接")
                 while True:
                     if ser.in_waiting > 0:
@@ -23,4 +23,5 @@ def read_data(timeout=1):
                                     yield None
         except Exception as e:
             print(f"电流检测原件串口连接断开，尝试重新连接... {e}")
+            yield None
             time.sleep(1)
