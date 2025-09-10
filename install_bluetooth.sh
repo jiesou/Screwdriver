58959f83685e752794952e763642297052744e4b
jiesou <84175239+jiesou@users.noreply.github.com>
2025-09-10 06:35:49 +0000

    devops: 迁移到 wayland

--- install_bluetooth.sh
+++ install_bluetooth.sh
@@ -2,5 +2,9 @@
 
 sudo apt update
 sudo apt install libgirepository1.0-dev libcairo2-dev python3-gi python3-gi-cairo gir1.2-gtk-3.0 libdbus-1-dev python3-dbus libgl1-mesa-glx -y
+sudo apt install freeglut3-dev -y # fix ImportError: libEGL.so.1
+# For X11: sudo apt install libxcb-cursor0 libxcb-xinerama0 libxcb-randr0 -y
 pip3 install -r requirements.txt
 export QT_SCALE_FACTOR=1.5
+export QT_QPA_PLATFORM=wayland-egl
+export XDG_SESSION_TYPE=wayland
