bb89792311cf7b6b6f003b782828b5aa2167c06c
jiesou <84175239+jiesou@users.noreply.github.com>
2025-09-10 07:36:56 +0000

    fix: 电流传感器通讯

--- processor/current/communication.py
+++ processor/current/communication.py
@@ -7,21 +7,25 @@ import os
 response = None
 results = []
 
-def open_connection():
-    global response
-    if response is None:
-        try:
-            response = requests.get(
-                os.getenv("CURRENT_SENSOR_HTTP", "http://ESP-1720F6.lan/status"),
-                stream=True,
-                timeout=1
-            )
-            response.raise_for_status()
-            print("[CurrentSensor] 网络已连接")
-        except Exception as e:
-            print(f"[CurrentSensor] 网络连接失败: {e}")
-            time.sleep(1)
-            response = None
+    def open_connection(self):
+        if self.response is None:
+            try:
+                self.response = requests.get(
+                    os.getenv("CURRENT_SENSOR_HTTP", "http://ESP-1720F6.lan/status"),
+                    stream=True,
+                    timeout=1
+                )
+                # 后端的 http 故障也直接抛出异常
+                self.response.raise_for_status()
+                print("[CurrentSensor] 网络已连接")
+            except Exception as e:
+                print(f"[CurrentSensor] 网络连接失败: {e}")
+                time.sleep(1)
+                self.response = None
+                self.open_connection()
+
+    def __init__(self):
+        self.open_connection()
 
 # 模块导入时就尝试建立连接
 open_connection()
@@ -45,7 +49,9 @@ def read_data():
                             continue
             except Exception as e:
                 print(f"[CurrentSensor] 网络连接断开: {e}")
-                response = None
+                time.sleep(1)
+                self.response = None
+                self.open_connection()
                 yield None
                 continue
 
